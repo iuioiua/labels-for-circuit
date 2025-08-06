f0b2372e5037ef04e35886dd61dd60f3426eabad
Asher Gomez <ashersaupingomez@gmail.com>
2025-08-07 09:47:39 +1000

    refactor: minor tweaks

--- main.ts
+++ main.ts
@@ -2,55 +2,10 @@ import { serveFile } from "@std/http/file-server";
 import { parse } from "@std/csv/parse";
 import { PDFDocument, StandardFonts } from "pdf-lib";
 
-const INCH_POINTS = 72; // 1" = 72 point
-const PAGE_WIDTH = 4 * INCH_POINTS; // 4" in points
-const PAGE_HEIGHT = 6 * INCH_POINTS; // 6" in points
-const TOP_MARGIN = 0.5 * INCH_POINTS; // 0.5" margin from the top
-
-interface ToLabelsConfig {
-  csv: string;
-  dateStr: string;
-  includeProduct: boolean;
-}
-
-async function toLabels(config: ToLabelsConfig): Promise<Uint8Array> {
-  const rows = parse(config.csv, { skipFirstRow: true });
-  const pdfDoc = await PDFDocument.create();
-  const courierFont = await pdfDoc.embedFont(StandardFonts.Courier);
-  const groupedRows = Object.groupBy(rows, (row) => row.driver);
-
-  for (const row of rows) {
-    const page = pdfDoc.addPage([PAGE_WIDTH, PAGE_HEIGHT]);
-    page.setFontSize(12);
-    page.setLineHeight(72 * 0.25);
-    const totalStops = groupedRows[row.driver]?.length || 0;
-    const lines = [
-      row.recipient_name,
-      row.address,
-      row.recipient_phone,
-      " ",
-      `Sender: ${row.seller_name}`,
-      `Notes: ${row.notes}`,
-      " ",
-      `Date: ${config.dateStr}`,
-      `Order: ${row.stop_number} of ${totalStops}`,
-      `Driver: ${row.driver}`,
-      config.includeProduct ? `Product: ${row.products}` : "",
-    ];
-
-    page.drawText(
-      lines.join("\n"),
-      {
-        font: courierFont,
-        x: INCH_POINTS * 0.25, // 0.25" left margin
-        y: PAGE_HEIGHT - TOP_MARGIN, // 0.25" from the top
-        maxWidth: PAGE_WIDTH - INCH_POINTS * 0.5, // 0.5" margin on each side
-      },
-    );
-  }
-
-  return pdfDoc.save();
-}
+const INCHES_IN_PTS = 72; // 1" = 72 point
+const PAGE_WIDTH = 4 * INCHES_IN_PTS; // 4" in points
+const PAGE_HEIGHT = 6 * INCHES_IN_PTS; // 6" in points
+const LEFT_MARGIN = 0.25 * INCHES_IN_PTS; // 0.25" margin on left side (right side is same)
 
 async function handler(request: Request): Promise<Response> {
   const { pathname } = new URL(request.url);
@@ -76,12 +31,47 @@ async function handler(request: Request): Promise<Response> {
   if (typeof date !== "string" || !date) {
     return new Response("Bad Request", { status: 400 });
   }
+  const dateStr = new Date(date).toLocaleDateString("en-AU");
 
-  const labelsPdf = await toLabels({
-    csv: await file.text(),
-    dateStr: new Date(date || Date.now()).toLocaleDateString("en-AU"),
-    includeProduct: formData.get("include-product") === "on",
-  });
+  const includeProduct = formData.get("include-product") === "on";
+
+  const csvStr = await file.text();
+  const stops = parse(csvStr, { skipFirstRow: true });
+  const stopsByDriver = Object.groupBy(stops, (stop) => stop.driver);
+
+  const pdfDoc = await PDFDocument.create();
+  const font = await pdfDoc.embedFont(StandardFonts.Courier);
+  for (const stop of stops) {
+    const page = pdfDoc.addPage([PAGE_WIDTH, PAGE_HEIGHT]);
+    page.setFontSize(12);
+    page.setLineHeight(72 * 0.25);
+    const totalStops = stopsByDriver[stop.driver]?.length || 0;
+    const lines = [
+      stop.recipient_name,
+      stop.address,
+      stop.recipient_phone,
+      " ",
+      `Sender: ${stop.seller_name}`,
+      `Notes: ${stop.notes}`,
+      " ",
+      `Date: ${dateStr}`,
+      `Order: ${stop.stop_number} of ${totalStops}`,
+      `Driver: ${stop.driver}`,
+      includeProduct ? `Product: ${stop.products}` : "",
+    ];
+
+    page.drawText(
+      lines.join("\n"),
+      {
+        font,
+        x: LEFT_MARGIN,
+        y: PAGE_HEIGHT - 0.5 * INCHES_IN_PTS,
+        maxWidth: PAGE_WIDTH - LEFT_MARGIN * 2,
+      },
+    );
+  }
+  const labelsPdf = await pdfDoc.save();
+
   return new Response(labelsPdf, {
     headers: {
       "Content-Type": "application/pdf",
